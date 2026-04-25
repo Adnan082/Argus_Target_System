731dde52938e81ee4e576de010d73e2fce125fad
Adnan Cheema <adnancheema917@gmail.com>
2026-04-25 15:45:28 +0100

    fix: save plots to PLOTS_DIR, correct image path, push plots to GitHub

--- scripts/ec2_eda.sh
+++ scripts/ec2_eda.sh
@@ -56,7 +56,7 @@ echo "=== Running EDA: $(date) ==="
 
 # Push results to GitHub
 echo "=== Pushing to GitHub: $(date) ==="
-git add notebooks/01_xview_eda.ipynb
+git add notebooks/01_xview_eda.ipynb notebooks/plots/
 git commit -m "EDA results: xView dataset analysis [automated EC2 run $(date +%Y-%m-%d)]"
 git push
 
